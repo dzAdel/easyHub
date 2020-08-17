f53f78e7c3db4e86cb8247b41d199cf49b1a7fbf
Khaled BOUMEKOUEZ <chez.adel@outlook.fr>
2020-08-17 14:16:44 +0100

    easyTest-0

---
+++ ezTest/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easyTest
+{
+    public abstract class UnitTest
+    {
+        public string UnitName { get; }
+
+        //protected:
+
+    }
+}
